274ca4b71f9d7717878bee240190a9099cb239a1
MrSurajPawar <surajspawar007@gmail.com>
2024-03-28 11:56:21 +0530

    new concept addon

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,21 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerName = useRef(); //new hook
+  const [enteredPlayerName, setEnterPlayerName] = useState(null);
+
+  function handleClick() {
+    setEnterPlayerName(playerName.current.value);
+    playerName.current.value = "";
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        {/* created the reference for the input element */}
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
